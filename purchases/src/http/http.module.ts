65d89653c5683cea690fc03b5ca176481005f1db
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-06-21 17:46:47 -0300

    Delete purchases directory

--- purchases/src/http/http.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TestController } from './test.controller';
-
-@Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [TestController],
-})
-export class HttpModule {}
