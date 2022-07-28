3df4c31d7f6761edec5ac13a19e994395b376e60
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 <hugovarellaa@gmail.com>
2022-07-28 15:53:17 -0300

    Importando fonts da aplicacao

--- ignite-timer/src/App.tsx
+++ ignite-timer/src/App.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
+import { GlobalStyles } from "./styles/GlobalStyles";
 import { defaultTheme } from "./styles/theme/default";
 
 export function App() {
@@ -9,6 +10,7 @@ export function App() {
       <Button />
       <Button />
       <Button />
+      <GlobalStyles />
     </ThemeProvider>
   );
 }
