a67286c8475eac1e2e0e2aab84609fb31be848e6
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 <hugovarellaa@gmail.com>
2022-07-28 15:45:25 -0300

    Type styled-components para themas

--- ignite-timer/src/components/Button/styles.ts
+++ ignite-timer/src/components/Button/styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const ButtonContainer = styled.button`
-  background-color: ${({ theme }) => theme.colors["green-500"]};
+  background-color: ${({ theme }) => theme.colors["red-700"]};
   border: 0;
   border-radius: 4px;
   padding: 10px;
