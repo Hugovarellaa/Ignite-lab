8553eadb42c5249a229280b683e015d812a8f16c
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 <hugovarellaa@gmail.com>
2022-07-29 07:49:15 -0300

    Estilos container Home + Form home

--- ignite-timer/src/pages/Home/index.tsx
+++ ignite-timer/src/pages/Home/index.tsx
@@ -1,9 +1,10 @@
-import { ContainerHome } from './styles'
+import { Play } from 'phosphor-react'
+import { ContainerHome, CountdownContainer, FormContainer } from './styles'
 
 export function Home() {
   return (
     <ContainerHome>
-      <form action="">
+      <FormContainer>
         <div>
           <label htmlFor="task">Vou trabalhar em</label>
           <input id="task" />
@@ -14,16 +15,19 @@ export function Home() {
           <span>minutos</span>
         </div>
 
-        <div>
+        <CountdownContainer>
           <span>0</span>
           <span>0</span>
           <span>:</span>
           <span>0</span>
           <span>0</span>
-        </div>
+        </CountdownContainer>
 
-        <button type="submit"></button>
-      </form>
+        <button type="submit">
+          <Play size={24} />
+          começar
+        </button>
+      </FormContainer>
     </ContainerHome>
   )
 }
--- ignite-timer/src/pages/Home/styles.ts
+++ ignite-timer/src/pages/Home/styles.ts
@@ -1,3 +1,31 @@
 import styled from 'styled-components'
 
-export const ContainerHome = styled.div``
+export const ContainerHome = styled.main`
+  height: 100%;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 3.5rem;
+  }
+`
+
+export const FormContainer = styled.form`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  color: ${({ theme }) => theme.colors['gray-100']};
+  font-size: 1.125rem;
+  font-weight: bold;
+  flex-wrap: wrap;
+`
+
+export const CountdownContainer = styled.div``
