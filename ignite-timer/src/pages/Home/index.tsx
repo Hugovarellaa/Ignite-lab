bb37702efb111d6c5d1c0b89dfacfad9bf6658a9
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 <hugovarellaa@gmail.com>
2022-07-29 11:20:10 -0300

    React-Hook-Form com zod para autenticacao

--- ignite-timer/src/pages/Home/index.tsx
+++ ignite-timer/src/pages/Home/index.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable prettier/prettier */
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Play } from "phosphor-react";
 import { useForm } from "react-hook-form";
+import * as zod from "zod";
 
 import {
   ContainerHome,
@@ -12,12 +14,31 @@ import {
   TaskInput
 } from "./styles";
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, "Informe a tarefa"),
+  minutesAmount: zod
+    .number()
+    .min(5, "O Ciclo precisa ter no mínimo 05 minutos")
+    .max(60, "O Ciclo precisa ser de máximo 60 minutos"),
+});
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
+
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm();
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: "",
+      minutesAmount: 0 ,
+    },
+  });
+
   const task = watch("task");
   const isSubmitDisabled = !task;
 
-  function handleCreateNewCycle(data: any) {
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    reset()
     console.log(data);
   }
 
