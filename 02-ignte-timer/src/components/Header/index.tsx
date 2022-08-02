3bfcaccd5eed6b8106421dbad18eb020c75c1f84
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 <hugovarellaa@gmail.com>
2022-08-02 14:29:06 -0300

    Header e Layout

--- 02-ignte-timer/src/components/Header/index.tsx
+++ 02-ignte-timer/src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { Scroll, Timer } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
 import logoImg from '../../assets/logo.svg'
 import { HeaderContainer } from './styles'
 
@@ -6,8 +8,12 @@ export function Header() {
     <HeaderContainer>
       <img src={logoImg} alt="" />
       <nav>
-        <a href="">Timer</a>
-        <a href="">Hsitory</a>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
       </nav>
     </HeaderContainer>
   )
