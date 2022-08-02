78ad7fd0c60d4a90563dcb4582861013491f6152
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 <hugovarellaa@gmail.com>
2022-08-02 13:34:26 -0300

    Configurando React-Router-Dom

--- 02-ignte-timer/src/App.tsx
+++ 02-ignte-timer/src/App.tsx
@@ -1,11 +1,15 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
 import { GlobalStyles } from './styles/GlobalStyles'
 import { defaultTheme } from './styles/theme/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>ola mundo</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
 
       <GlobalStyles />
     </ThemeProvider>
