d6e2cf2d70cfa4e3d6fd5580f915e3c79693a36a
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 <hugovarellaa@gmail.com>
2022-08-02 15:44:52 -0300

    History page

--- 02-ignte-timer/src/pages/History/index.tsx
+++ 02-ignte-timer/src/pages/History/index.tsx
@@ -1,7 +1,77 @@
+import { HistoryContainer, HistoryList } from './styles'
+
 export function History() {
   return (
-    <div>
-      <h1>History</h1>
-    </div>
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Inicio</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Projeto 1</td>
+              <td>20 minutos</td>
+              <td>há cerca de 2 meses</td>
+              <td>
+                <span>Concluído</span>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Projeto 1</td>
+              <td>20 minutos</td>
+              <td>há cerca de 2 meses</td>
+              <td>
+                <span>Concluído</span>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Projeto 1</td>
+              <td>20 minutos</td>
+              <td>há cerca de 2 meses</td>
+              <td>
+                <span>Concluído</span>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Projeto 1</td>
+              <td>20 minutos</td>
+              <td>há cerca de 2 meses</td>
+              <td>
+                <span>Concluído</span>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Projeto 1</td>
+              <td>20 minutos</td>
+              <td>há cerca de 2 meses</td>
+              <td>
+                <span>Concluído</span>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Projeto 1</td>
+              <td>20 minutos</td>
+              <td>há cerca de 2 meses</td>
+              <td>
+                <span>Concluído</span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
   )
 }
