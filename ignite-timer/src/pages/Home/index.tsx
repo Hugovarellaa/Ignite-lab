dd55915b5654a68c72da392042b8b7a2eb656e06
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 <hugovarellaa@gmail.com>
2022-08-24 16:21:04 -0300

    Estrutura Home

--- ignite-timer/src/pages/Home/index.tsx
+++ ignite-timer/src/pages/Home/index.tsx
@@ -1,7 +1,36 @@
+import { Play } from 'phosphor-react'
+import {
+  CountDouwnContainer,
+  FormContainer,
+  HomeContainer,
+  Separator
+} from './styles'
+
 export function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em </label>
+          <input type="text" id="task" />
+          <label htmlFor="minutesAmountDuration">durante</label>
+          <input type="number" id="minutesAmountDuration" />
+          <span>minutos</span>
+        </FormContainer>
+
+        <CountDouwnContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDouwnContainer>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
   )
 }
