c0bb9fdf2c036a2a74a24dd35d35c68d61d646ad
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 <hugovarellaa@gmail.com>
2022-08-26 16:19:54 -0300

    Estrutura do projeto

---
+++ 02-ignite-timer-com-typescript/src/styles/GlobalStyles.ts
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: ${(props) => props.theme['gray-900']};
+    color: ${(props) => props.theme['gray-100']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, button, input, select, textarea {
+    font-family: "Roboto", sans-serif;
+    font-size: 1rem;
+    font-weight: 400;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  [disabled]{
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+`
