15174f460c62272d0e10e108875aeb77b9c0cbe0
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 <hugovarellaa@gmail.com>
2022-08-31 10:03:39 -0300

    Estlio do formulario e countdown

--- ignite-timer/src/pages/Home/index.tsx
+++ ignite-timer/src/pages/Home/index.tsx
@@ -1,7 +1,42 @@
+/* eslint-disable prettier/prettier */
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+  StartCountdownButton
+} from './styles'
+
 export function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input
+            type="text"
+            id="task"
+            placeholder="Dê um nome para o seu projeto"
+          />
+          <label htmlFor="minutesAmount">Durante</label>
+          <input type="number" id="minutesAmount" placeholder="00" />
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton>
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
   )
 }
