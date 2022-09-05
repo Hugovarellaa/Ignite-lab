159d03052faa1e3b69821addee94240af46a9700
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 <hugovarellaa@gmail.com>
2022-09-05 18:58:43 -0300

    Styled-components + estilos globais

---
+++ ignite-timer/src/App.tsx
@@ -0,0 +1,12 @@
+import { ThemeProvider } from "styled-components"
+import { GlobalStyles } from "./styles/global/GlobalStyles"
+import { defaultTheme } from "./styles/theme/default"
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>App</h1>
+      <GlobalStyles />
+    </ThemeProvider>
+  )
+}
