7df0635c6dbc5e2f7c71897039a629c85b933c89
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 <hugovarellaa@gmail.com>
2022-09-05 20:33:44 -0300

    Layout Home Page

--- ignite-timer/src/pages/Home/index.tsx
+++ ignite-timer/src/pages/Home/index.tsx
@@ -1,9 +1,63 @@
-import { HomeContainer } from './styles'
+/* eslint-disable prettier/prettier */
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmount,
+  Separator,
+  StartCountdownButton,
+  TaskInput
+} from './styles'
+
 
 export function Home() {
   return (
     <HomeContainer>
-      <h1>Home</h1>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            type="text"
+            id="task"
+            list="task-suggest"
+            placeholder="Dê um nome para o seu projeto"
+          />
+
+          <datalist id="task-suggest">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Banana" />
+            <option value="Alguma coisa" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmount
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            max={60}
+            min={5}
+            step={5}
+
+          />
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton type="submit" >
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
     </HomeContainer>
   )
 }
