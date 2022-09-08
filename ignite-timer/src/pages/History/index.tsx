4813474d17411dee76454e5518a7bc87ee15fa45
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 <hugovarellaa@gmail.com>
2022-09-08 11:04:32 -0300

    Layout da Histoy page

--- ignite-timer/src/pages/History/index.tsx
+++ ignite-timer/src/pages/History/index.tsx
@@ -1,7 +1,65 @@
+import { HistoryContainer, HistoryList } from './styles'
+
 export function History() {
   return (
-    <div>
-      <h1>History</h1>
-    </div>
+    <HistoryContainer>
+      <h1>Meu Histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 Minutos</td>
+              <td>Há cerca de 2 minutos</td>
+              <td>Concluído</td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 Minutos</td>
+              <td>Há cerca de 2 minutos</td>
+              <td>Concluído</td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 Minutos</td>
+              <td>Há cerca de 2 minutos</td>
+              <td>Concluído</td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 Minutos</td>
+              <td>Há cerca de 2 minutos</td>
+              <td>Concluído</td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 Minutos</td>
+              <td>Há cerca de 2 minutos</td>
+              <td>Concluído</td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 Minutos</td>
+              <td>Há cerca de 2 minutos</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
   )
 }
