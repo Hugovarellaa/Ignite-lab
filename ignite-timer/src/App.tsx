1b2cb597dc23fac6bfc9df43d93f235540883d2a
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-04 09:07:20 -0300

    Configuração de rotas com React-router-dom

--- ignite-timer/src/App.tsx
+++ ignite-timer/src/App.tsx
@@ -1,14 +1,16 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global/GlobalStyles'
 import { defaultTheme } from './styles/theme/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div>
-        <h1>Hello world</h1>
+      <BrowserRouter>
+        <Router />
         <GlobalStyle />
-      </div>
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
