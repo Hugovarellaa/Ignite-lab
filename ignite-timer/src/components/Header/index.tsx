7ef31efe58d51a1a7ebbf797d5717ba55abee344
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-04 11:04:01 -0300

    Finalizando o header

--- ignite-timer/src/components/Header/index.tsx
+++ ignite-timer/src/components/Header/index.tsx
@@ -1,9 +1,21 @@
+import { Scroll, Timer } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
+import logoImg from '../../assets/Logo.svg'
 import { HeaderConttainer } from './styles'
 
 export function Header() {
   return (
     <HeaderConttainer>
-      <h1>Header</h1>
+      <img src={logoImg} alt="" />
+
+      <nav>
+        <NavLink to="/" end title="Home">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" end title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
     </HeaderConttainer>
   )
 }
