73ae73d5945f55ec554867f35cbb0d349d9e4817
hugo <hugovarellaa@gmail.com>
2023-01-03 17:20:46 -0300

    Frame Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,32 @@
+import { Play } from 'phosphor-react'
+import { HomeContainer } from './styles'
+
 export function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <HomeContainer>
+      <form>
+        <div>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input type="text" id="task" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <input type="number" id="minutesAmount" />
+          <span>minutos.</span>
+        </div>
+
+        <div>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+        </div>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
   )
 }
