ff75f39b3ff50a9067c21d558338cd2c1d91a96f
hugo <hugovarellaa@gmail.com>
2023-01-03 16:24:13 -0300

    Global Styles

--- src/styles/global/GlobalStyles.ts
+++ src/styles/global/GlobalStyles.ts
@@ -2,6 +2,18 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
 
+ @media(max-width: 1080px){
+    html {
+      font-size: 100%;
+    }
+  }
+  @media(max-width: 720px){
+    html {
+      font-size: 87.5%;
+    }
+  }
+
+
   * {
     margin: 0;
     padding: 0;
@@ -9,8 +21,33 @@ export const GlobalStyles = createGlobalStyle`
     -webkit-font-smoothing: antialiased;
   }
 
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme["green-500"]}
+  }
+
   body {
     background-color: ${({ theme }) => theme["gray-900"]};
     color: ${({ theme }) => theme["gray-100"]};
+  }
+
+  body, button ,input, select , textarea {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  [disabled]{
+    cursor: not-allowed;
+    opacity: 0.7;
   }
 `;
