a2df83ed5e8922986d2a2c8ab99155db05cb8502
hugo <hugovarellaa@gmail.com>
2023-01-03 16:17:23 -0300

    Using GlobalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./styles/global/GlobalStyles";
 import { defaultTheme } from "./styles/theme";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <h1>First render</h1>
+      <GlobalStyles />
     </ThemeProvider>
   );
 }
