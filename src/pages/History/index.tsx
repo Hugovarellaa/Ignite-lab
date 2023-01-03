c429070cba998e877bdc824fd6db2564cee2887b
hugo <hugovarellaa@gmail.com>
2023-01-03 19:50:57 -0300

    Layout History page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,7 +1,136 @@
+import { HistoryContainer, HistoryList, HistoryStatus } from './styles'
+
 export function History() {
   return (
-    <div>
-      <h1>History</h1>
-    </div>
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa 1</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <HistoryStatus status_color="green">Concluído</HistoryStatus>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 1</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <HistoryStatus status_color="green">Concluído</HistoryStatus>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 1</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <HistoryStatus status_color="yellow">Concluído</HistoryStatus>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 1</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <HistoryStatus status_color="red">Concluído</HistoryStatus>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 1</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <HistoryStatus status_color="red">Concluído</HistoryStatus>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 1</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <HistoryStatus status_color="green">Concluído</HistoryStatus>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 1</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <HistoryStatus status_color="green">Concluído</HistoryStatus>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 1</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <HistoryStatus status_color="red">Concluído</HistoryStatus>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 1</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <HistoryStatus status_color="red">Concluído</HistoryStatus>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 1</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <HistoryStatus status_color="green">Concluído</HistoryStatus>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 1</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <HistoryStatus status_color="yellow">Concluído</HistoryStatus>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 1</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <HistoryStatus status_color="red">Concluído</HistoryStatus>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 1</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <HistoryStatus status_color="red">Concluído</HistoryStatus>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 1</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <HistoryStatus status_color="green">Concluído</HistoryStatus>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
   )
 }
