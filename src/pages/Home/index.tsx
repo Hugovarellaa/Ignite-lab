356d3ef9bcefe8bbd06c59e862c4f29d3e92acd1
hugo <hugovarellaa@gmail.com>
2023-01-04 14:57:47 -0300

    Bug 1 - 2 timer reducing

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -74,12 +74,17 @@ export default function Home() {
   }
 
   useEffect(() => {
+    let interval: number
+
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate),
         )
       }, 1000)
+    }
+    return () => {
+      clearInterval(interval)
     }
   }, [activeCycle])
 
